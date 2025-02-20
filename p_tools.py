f574b6689685ec11e4cc1949bc0242e6ae3508bd
IsuiGit <gitpusherisui@gmail.com>
2025-02-20 17:19:14 +0300

    v1.1.2(2225)a update

--- p_tools.py
+++ p_tools.py
@@ -4,7 +4,6 @@ import sys
 import os
 
 INPUTS = sys.argv
-EXCEPT_NAME = ['svchost.exe', 'python.exe']
 
 def loadJSON(path):
     with open(path, "r", encoding="utf-8") as f:
@@ -19,25 +18,12 @@ def serviceFuncTest(name=INPUTS[1], case=INPUTS[2]):
     finally:
         globals()['psutil'] = importlib.import_module('psutil')
     try:
-        pid = psutil.win_service_get(name).pid()
-        exe = psutil.Process(pid).exe()
-        if not exe or os.path.split(exe)[1] in EXCEPT_NAME:
-            return f"{INPUTS[1]}: the path to the executable file for the selected service was not found or it is internal to the system {EXCEPT_NAME}"
-        else:
-            serviceTestCase = loadJSON(case)
-            try:
-                comma = serviceTestCase["flags"]
-            except KeyError:
-                return f"Test case must include flag section with text params"
-            from subprocess import Popen, PIPE
-            try:
-                p = Popen([exe, *comma], stdout=PIPE)
-                response = p.communicate()[0]
-                return response
-            except Exception as e:
-                return repr(e)
+        service = psutil.win_service_get(name)
+        return f"binpath: {service.binpath()}"
     except psutil.NoSuchProcess:
         return f"No service named {INPUTS[1]}"
+    except psutil.AccessDenied:
+        return f"Access denied {INPUTS[1]}"
 
 if __name__=='__main__':
     serviceFuncTestStdOut = serviceFuncTest()
